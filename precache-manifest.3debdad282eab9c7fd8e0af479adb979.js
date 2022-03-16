6e430caa49c397c5b00e18f8479c956a790c23c1
noradowndog <50186255+noradowndog@users.noreply.github.com>
2022-03-16 10:22:22 -0700

    Updates

---
+++ precache-manifest.3debdad282eab9c7fd8e0af479adb979.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a897c1d1304d77a8bcdd22383df97f",
+    "url": "/nkako1/portfolio/index.html"
+  },
+  {
+    "revision": "fe5eed6e1a9c65a22bed",
+    "url": "/nkako1/portfolio/static/css/main.7b6a5259.chunk.css"
+  },
+  {
+    "revision": "ba3e46bb2c3ea4b33cd0",
+    "url": "/nkako1/portfolio/static/js/2.611bb18a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/nkako1/portfolio/static/js/2.611bb18a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5eed6e1a9c65a22bed",
+    "url": "/nkako1/portfolio/static/js/main.cd9bb57a.chunk.js"
+  },
+  {
+    "revision": "38cc072e4534326fef65",
+    "url": "/nkako1/portfolio/static/js/runtime-main.8551c3cc.js"
+  },
+  {
+    "revision": "3495dd7c8769595042360185633bf699",
+    "url": "/nkako1/portfolio/static/media/NoraKakoResume.3495dd7c.pdf"
+  },
+  {
+    "revision": "d1f45015281d3b292d819b22f2a17e94",
+    "url": "/nkako1/portfolio/static/media/downdog.d1f45015.png"
+  },
+  {
+    "revision": "6167c41f3936df27cbe44f49c1a68355",
+    "url": "/nkako1/portfolio/static/media/eiq_mobility.6167c41f.png"
+  },
+  {
+    "revision": "d058a9d9084afc07334f34b229d750b6",
+    "url": "/nkako1/portfolio/static/media/kapwing.d058a9d9.png"
+  },
+  {
+    "revision": "223350b0acb0149ca9dcc3ab4419fc93",
+    "url": "/nkako1/portfolio/static/media/mindful_mamas.223350b0.png"
+  },
+  {
+    "revision": "bb4820c9b2b528f0c2ce2292cdac1fa3",
+    "url": "/nkako1/portfolio/static/media/reduct.bb4820c9.png"
+  },
+  {
+    "revision": "11c27fff24414ab3fb719a1d925091df",
+    "url": "/nkako1/portfolio/static/media/retain.11c27fff.png"
+  },
+  {
+    "revision": "89a033808ea62ee908f54c79d1047e2a",
+    "url": "/nkako1/portfolio/static/media/thumbnail_downdog.89a03380.png"
+  },
+  {
+    "revision": "7f7e498e2e7b902a65492a9c997b7013",
+    "url": "/nkako1/portfolio/static/media/thumbnail_eiq_mobility.7f7e498e.png"
+  },
+  {
+    "revision": "e0aa5fea843caa92beff9dd29d666edf",
+    "url": "/nkako1/portfolio/static/media/thumbnail_kapwing.e0aa5fea.png"
+  },
+  {
+    "revision": "7aad94b2807584a6276a5b5c54eedcf8",
+    "url": "/nkako1/portfolio/static/media/thumbnail_mindful_mamas.7aad94b2.png"
+  },
+  {
+    "revision": "4f79784960967fbde2a8d6d362a960df",
+    "url": "/nkako1/portfolio/static/media/thumbnail_reduct.4f797849.png"
+  },
+  {
+    "revision": "763020bcc315f029ee94a21dbcd895d3",
+    "url": "/nkako1/portfolio/static/media/thumbnail_retain.763020bc.png"
+  }
+]);
